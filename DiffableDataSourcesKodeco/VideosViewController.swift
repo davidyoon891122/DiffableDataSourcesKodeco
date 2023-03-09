05a9111618f4ff73a0bd1460539762a911d01634
Davidyoon <davidyoon@koreainvestment.com>
2023-03-09 14:01:20 +0900

    Set a default skeleton
    * Removed the main storyboard
    * Bound window to scene view

---
+++ DiffableDataSourcesKodeco/VideosViewController.swift
@@ -0,0 +1,26 @@
+//
+//  VideosViewController.swift
+//  DiffableDataSourcesKodeco
+//
+//  Created by jiwon Yoon on 2023/03/09.
+//
+
+import UIKit
+
+class VideosViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSearchController()
+    }
+
+
+}
+
+private extension VideosViewController {
+    func configureSearchController() {
+        
+    }
+}
+
